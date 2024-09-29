7f06dc94010df0ee57d420c3e51cf8f6b2c752f9
Ashley H <ashleyhughes223@gmail.com>
2024-09-29 22:17:24 +0100

    - Added about BOOSE Call - Updated README file to correct spelling of my own name

--- ASE Assignment Ashley Hughes/Form1.cs
+++ ASE Assignment Ashley Hughes/Form1.cs
@@ -1,3 +1,9 @@
+using System.Diagnostics;
+using BOOSE;
+
+
+
+
 namespace ASE_Assignment_Ashley_Hughes
 {
     public partial class Form1 : Form
@@ -5,6 +11,12 @@ namespace ASE_Assignment_Ashley_Hughes
         public Form1()
         {
             InitializeComponent();
+            Debug.WriteLine(AboutBOOSE.about());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/979EF3Rq)
 # ASE Portfolio
-Fill in the fields below.
 
-## Asheley Hughes
+## Ashley Hughes
 ### Group 1
 ### Duncan Mullier
 
