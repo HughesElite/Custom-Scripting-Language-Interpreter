d4e30bbed1fce6142f2b7045089a6f0b5e26452d
Ashley H <100367853+HughesElite@users.noreply.github.com>
2025-03-24 20:46:52 +0000

    Updated application to use 'AppWrite' which allowed for removing many overly complicated lines of code for the 'Write' Command to work

--- ASE Assignment Ashley Hughes/AppArray.cs
+++ ASE Assignment Ashley Hughes/AppArray.cs
@@ -7,44 +7,7 @@ using BOOSE;
 
 namespace ASE_Assignment_Ashley_Hughes
 {
-    public class AppArray : Evaluation
-    {
-        private int[,] intArray;
-        private double[,] realArray;
-        private int rows;
-        private int columns;
-        private string type;
-
-        public int Rows => rows;
-        public int Columns => columns;
-
-        public AppArray(string type, int rows, int columns = 1)
-        {
-            this.type = type.ToLower();
-            this.rows = rows;
-            this.columns = columns;
-
-            if (type == "int")
-                intArray = new int[rows, columns];
-            else if (type == "real")
-                realArray = new double[rows, columns];
-            else
-                throw new ArgumentException("Invalid type. Use 'int' or 'real'.");
-        }
-
-        public void SetIntValue(int row, int col, int value)
-        {
-            ValidateIndices(row, col);
-            intArray[row, col] = value;
-        }
-
-        public int GetIntValue(int row, int col)
-        {
-            ValidateIndices(row, col);
-            return intArray[row, col];
-        }
-
-        public void SetRealValue(int row, int col, double value)
+    public class AppArray : BOOSE.Array
         {
             ValidateIndices(row, col);
             realArray[row, col] = value;
@@ -56,10 +19,4 @@ namespace ASE_Assignment_Ashley_Hughes
             return realArray[row, col];
         }
 
-        private void ValidateIndices(int row, int col)
-        {
-            if (row >= rows || col >= columns || row < 0 || col < 0)
-                throw new IndexOutOfRangeException("Array index out of bounds.");
-        }
-    }
 }
